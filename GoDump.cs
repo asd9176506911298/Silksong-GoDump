ee37cec80521f11f0ea7e82a5f9bfb15dae9d92e
Yuki.kaco <a0936660023@gmail.com>
2025-09-13 03:10:22 +0800

    Add project files.

---
+++ GoDump.cs
@@ -0,0 +1,321 @@
+﻿using BepInEx;
+using UnityEngine.Windows;
+using UnityEngine;
+using Input = UnityEngine.Input;
+using static UnityEngine.RectTransform;
+using System.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using GODump;
+using static GODump.SpriteDump;
+using System.IO;
+using System.Reflection;
+using BepInEx.Logging;
+
+namespace GoDump
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class GoDump : BaseUnityPlugin
+    {
+        public static GoDump Instance { get; private set; }
+
+        private const string modGUID = "Yuki.GoDump";
+        private const string modName = "GoDump";
+        private const string modVersion = "1.0.0";
+
+        private static readonly string _spritePath = Application.persistentDataPath + "/sprites/";
+        private static readonly string _atlasPath = Application.persistentDataPath + "/atlases/";
+
+        private List<tk2dSpriteCollectionData> clns;
+        private List<tk2dSpriteAnimation> anims;
+        private string[] animNames;
+        private int num;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {modGUID} is loaded!");
+        }
+
+        private void Start()
+        {
+            Instance = this;
+
+            clns = new List<tk2dSpriteCollectionData>();
+            anims = new List<tk2dSpriteAnimation>();
+            animNames = new string[] { };
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F4))
+            {
+                var animation = HeroController.instance.GetComponent<tk2dSpriteAnimator>().Library;
+                anims.Add(animation);
+                animNames = anims.Select(a => a.name).ToArray();
+                StartCoroutine(HornetSprite());
+            }
+
+            if (Input.GetKeyDown(KeyCode.F5))
+            {
+                //Texture2D texture = new Texture2D(2, 2);
+                //byte[] fileData = File.ReadAllBytes("C:\\Users\\a0936\\AppData\\LocalLow\\Team Cherry\\testPack\\Knight\\Knight_atlas2.png");
+                //ImageConversion.LoadImage(texture, fileData); // Explicitly call from ImageConversion
+                //HeroController.instance.GetComponent<tk2dSprite>().GetCurrentSpriteDef().material.mainTexture = texture;
+                foreach(var x in HeroController.instance.GetComponent<tk2dSprite>().Collection.materials)
+                {
+                    Logger.LogInfo(x.mainTexture.name);
+
+                    Texture2D texture = new Texture2D(2, 2);
+                    texture.name = x.mainTexture.name;
+                    byte[] fileData = File.ReadAllBytes($"C:\\Users\\a0936\\AppData\\LocalLow\\Team Cherry\\testPack\\Knight\\Knight_{x.mainTexture.name}.png");
+                    ImageConversion.LoadImage(texture, fileData); // Explicitly call from ImageConversion
+
+                    Logger.LogInfo("SetTexture");
+                    x.mainTexture = texture;
+                }
+            }
+
+            if (HeroController.instance)
+            {
+                if (HeroController.instance.GetComponent<tk2dSpriteAnimator>())
+                {
+                    //Logger.LogInfo(HeroController.instance.GetComponent<tk2dSpriteAnimator>().CurrentClip.name);
+                }
+            }       
+        }
+
+        private IEnumerator HornetSprite()
+        {
+            num = 0;
+            foreach (var animL in anims)
+            {
+                if (animNames.Contains(animL.name))
+                {
+                    int i = 0;
+                    SpriteInfo spriteInfo = new SpriteInfo();
+                    Logger.LogInfo("Begin Dumping sprites in tk2dSpriteAnimator [" + animL.name + "].");
+                    foreach (tk2dSpriteAnimationClip clip in animL.clips)
+                    {
+                        Logger.LogInfo(clip.name);
+                        //if (clip.name == "SlashAlt")
+                        //    break;
+
+                        i++;
+                        int j = -1;
+                        float Xmax = -10000f;
+                        float Ymax = -10000f;
+                        float Xmin = 10000f;
+                        float Ymin = 10000f;
+                        foreach (tk2dSpriteAnimationFrame frame in clip.frames)
+                        {
+                            tk2dSpriteDefinition tk2DSpriteDefinition = frame.spriteCollection.spriteDefinitions[frame.spriteId];
+                            Vector3[] pos = tk2DSpriteDefinition.positions;
+
+                            float xmin = pos.Min(v => v.x);
+                            float ymin = pos.Min(v => v.y);
+                            float xmax = pos.Max(v => v.x);
+                            float ymax = pos.Max(v => v.y);
+
+                            Xmin = Xmin < xmin ? Xmin : xmin;
+                            Ymin = Ymin < ymin ? Ymin : ymin;
+                            Xmax = Xmax > xmax ? Xmax : xmax;
+                            Ymax = Ymax > ymax ? Ymax : ymax;
+
+                        }
+                        foreach (tk2dSpriteAnimationFrame frame in clip.frames)
+                        {
+                            j++;
+
+                            if (clip.name == "")
+                            {
+                                clip.name = "Focus Air";
+                            }
+
+                            tk2dSpriteDefinition tk2DSpriteDefinition = frame.spriteCollection.spriteDefinitions[frame.spriteId];
+                            Vector2[] uv = tk2DSpriteDefinition.uvs;
+                            Vector3[] pos = tk2DSpriteDefinition.positions;
+                            Texture texture = tk2DSpriteDefinition.material.mainTexture;
+                            Texture2D texture2D = SpriteDump.TextureReadHack((Texture2D)texture);
+
+                            string collectionname = frame.spriteCollection.spriteCollectionName + "_" + tk2DSpriteDefinition.material.mainTexture.name;
+                            string path = _spritePath + animL.name + "/0.Atlases/" + collectionname + ".png";
+                            string path0 = _spritePath + animL.name + "/" + String.Format("{0:D3}", i) + "." + clip.name + "/" + collectionname + ".png";
+                            string path1 = _spritePath + animL.name + "/" + String.Format("{0:D3}", i) + "." + clip.name + "/" + String.Format("{0:D3}", i) + "-" + String.Format("{0:D2}", j) + "-" + String.Format("{0:D3}", frame.spriteId) + "_position.png";
+                            string path2r = animL.name + "/" + String.Format("{0:D3}", i) + "." + clip.name + "/" + String.Format("{0:D3}", i) + "-" + String.Format("{0:D2}", j) + "-" + String.Format("{0:D3}", frame.spriteId) + ".png";
+                            string path2 = _spritePath + path2r;
+
+                            bool flipped = tk2DSpriteDefinition.flipped == tk2dSpriteDefinition.FlipMode.Tk2d;
+
+                            float xmin = pos.Min(v => v.x);
+                            float ymin = pos.Min(v => v.y);
+                            float xmax = pos.Max(v => v.x);
+                            float ymax = pos.Max(v => v.y);
+
+
+
+                            int x1 = (int)(uv.Min(v => v.x) * texture2D.width);
+                            int y1 = (int)(uv.Min(v => v.y) * texture2D.height);
+                            int x2 = (int)(uv.Max(v => v.x) * texture2D.width);
+                            int y2 = (int)(uv.Max(v => v.y) * texture2D.height);
+
+                            // symmetry transformation
+                            int x11 = x1;
+                            int y11 = y1;
+                            int x22 = x2;
+                            int y22 = y2;
+                            if (flipped)
+                            {
+                                x22 = y2 + x1 - y1;
+                                y22 = x2 - x1 + y1;
+                            }
+
+                            int x3 = (int)((Xmin - Xmin) / tk2DSpriteDefinition.texelSize.x);
+                            int y3 = (int)((Ymin - Ymin) / tk2DSpriteDefinition.texelSize.y);
+                            int x4 = (int)((Xmax - Xmin) / tk2DSpriteDefinition.texelSize.x);
+                            int y4 = (int)((Ymax - Ymin) / tk2DSpriteDefinition.texelSize.y);
+
+                            int x5 = (int)((xmin - Xmin) / tk2DSpriteDefinition.texelSize.x);
+                            int y5 = (int)((ymin - Ymin) / tk2DSpriteDefinition.texelSize.y);
+                            int x6 = (int)((xmax - Xmin) / tk2DSpriteDefinition.texelSize.x);
+                            int y6 = (int)((ymax - Ymin) / tk2DSpriteDefinition.texelSize.y);
+
+                            RectP uvpixel = new RectP(x1, y1, x2 - x1 + 1, y2 - y1 + 1);
+                            RectP posborder = new RectP(x11 - x5 + x3, y11 - y5 + y3, x4 - x3 + 1, y4 - y3 + 1);
+                            RectP uvpixelr = new RectP(x5 - x3, y5 - y3, x22 - x11 + 1, y22 - y11 + 1);
+
+
+                            if (!File.Exists(path) && true) // GODump.instance.GlobalSettings.DumpAtlasOnce
+                            {
+                                SpriteDump.SaveTextureToFile(texture2D, path);
+                                num++;
+                            }
+                            if (!File.Exists(path0) && false) // GODump.instance.GlobalSettings.DumpAtlasAlways
+                            {
+                                SpriteDump.SaveTextureToFile(texture2D, path0);
+                                num++;
+                            }
+                            if (!File.Exists(path1) && false) // GODump.instance.GlobalSettings.DumpPosition
+                            {
+                                Texture2D subposition2D = SpriteDump.SubTexturePosition(texture2D, uvpixel);
+                                SpriteDump.SaveTextureToFile(subposition2D, path1);
+                                num++;
+                                UnityEngine.Object.DestroyImmediate(subposition2D);
+                            }
+                            if (true) // GODump.instance.GlobalSettings.DumpSpriteInfo
+                            {
+                                spriteInfo.Add(frame.spriteId, x1, y1, uvpixelr.x, uvpixelr.y, uvpixelr.width, uvpixelr.height, collectionname, path2r, flipped);
+                            }
+                            if (!File.Exists(path2))
+                            {
+                                try
+                                {
+                                    // 檢查 uvpixel 是否超出 texture2D 範圍
+                                    if (uvpixel.x < 0 || uvpixel.y < 0 ||
+                                        uvpixel.x + uvpixel.width > texture2D.width ||
+                                        uvpixel.y + uvpixel.height > texture2D.height)
+                                    {
+                                        Logger.LogWarning($"Texture rectangle out of bounds for {path2r}. Adjusting uvpixel.");
+                                        // 裁剪 uvpixel 範圍以確保不超出紋理尺寸
+                                        uvpixel.x = Mathf.Max(0, uvpixel.x);
+                                        uvpixel.y = Mathf.Max(0, uvpixel.y);
+                                        uvpixel.width = Mathf.Min(uvpixel.width, texture2D.width - uvpixel.x);
+                                        uvpixel.height = Mathf.Min(uvpixel.height, texture2D.height - uvpixel.y);
+
+                                        // 如果裁剪後寬高無效，跳過處理
+                                        if (uvpixel.width <= 0 || uvpixel.height <= 0)
+                                        {
+                                            Logger.LogError($"Invalid texture rectangle after adjustment for {path2r}. Skipping.");
+                                            continue;
+                                        }
+                                    }
+
+                                    Texture2D subtexture2D = SpriteDump.SubTexture(texture2D, uvpixel);
+                                    if (flipped)
+                                    {
+                                        SpriteDump.Tk2dFlip(ref subtexture2D);
+                                    }
+
+                                    if (true) // SpriteSizeFix
+                                    {
+                                        Texture2D fixedtexture2D = SpriteDump.SpriteSizeFix(subtexture2D, uvpixelr, posborder);
+                                        SpriteDump.SaveTextureToFile(fixedtexture2D, path2);
+                                        UnityEngine.Object.DestroyImmediate(fixedtexture2D);
+                                    }
+                                    else
+                                    {
+                                        SpriteDump.SaveTextureToFile(subtexture2D, path2);
+                                    }
+                                    UnityEngine.Object.DestroyImmediate(subtexture2D);
+                                    num++;
+                                }
+                                catch (Exception ex)
+                                {
+                                    Texture2D subtexture2D = SpriteDump.SubTexture(texture2D, uvpixel);
+                                    if (flipped)
+                                    {
+                                        SpriteDump.Tk2dFlip(ref subtexture2D);
+                                    }
+
+                                    Texture2D fixedtexture2D = SpriteDump.SpriteSizeFixError(subtexture2D, uvpixelr, posborder);
+                                    SpriteDump.SaveTextureToFile(fixedtexture2D, path2);
+                                    UnityEngine.Object.DestroyImmediate(fixedtexture2D);
+                                    Logger.LogInfo($"{ex.ToString()} {path2r}");
+                                }
+                            }
+                            UnityEngine.Object.DestroyImmediate(texture2D);
+                        }
+                        //yield return new WaitForSeconds(1.0f);
+                    }
+
+                    string spriteinfopath = _spritePath + animL.name + "/0.Atlases/SpriteInfo.json";
+                    if (!File.Exists(spriteinfopath) && true) // GODump.instance.GlobalSettings.DumpSpriteInfo
+                    {
+                        using (FileStream fileStream = File.Create(spriteinfopath))
+                        {
+                            using (StreamWriter streamWriter = new StreamWriter(fileStream))
+                            {
+                                string value = JsonUtility.ToJson(spriteInfo, true);
+                                streamWriter.Write(value);
+                            }
+                        }
+                    }
+
+
+                    Logger.LogInfo("End Dumping sprites in tk2dSpriteAnimator [" + animL.name + "].");
+
+                }
+
+            }
+
+
+            Logger.LogInfo("End Dumping Sprite.png " + num + " sprites dumped.");
+            yield break;
+        }
+
+        private List<tk2dSpriteAnimation> GetUsedIns(tk2dSpriteCollectionData cln, List<tk2dSpriteAnimation> anims)
+        {
+            List<tk2dSpriteAnimation> used = new List<tk2dSpriteAnimation>();
+            foreach (tk2dSpriteAnimation anim in anims)
+            {
+                foreach (tk2dSpriteAnimationClip clip in anim.clips)
+                {
+                    foreach (tk2dSpriteAnimationFrame frame in clip.frames)
+                    {
+                        if (frame.spriteCollection.name == cln.name && !used.Contains(anim))
+                        {
+                            used.Add(anim);
+                        }
+                    }
+                }
+            }
+            return used;
+        }
+
+        public static void logInfo(string msg)
+        {
+            Instance.Logger.LogInfo(msg);
+        }
+    }
+}
---
+++ SpriteDump.cs
@@ -0,0 +1,154 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace GODump
+{
+    //Reference to SpriteDump.dll by KayDeeTee
+    //And have been modified a lot
+    static class SpriteDump
+    {
+        private static readonly Color[] colors = new Color[4096 * 4096];
+        public static void Tk2dFlip(ref Texture2D texture2D)
+        {
+            Texture2D flipped = new Texture2D(texture2D.height, texture2D.width);
+            for (int x = 0; x < texture2D.width; x++)
+            {
+                for (int y = 0; y < texture2D.height; y++)
+                {
+                    flipped.SetPixel(y, x, texture2D.GetPixel(x, y));
+                }
+            }
+            texture2D = flipped;
+        }
+        public static Texture2D TextureReadHack(Texture2D in_tex)
+        {
+            RenderTexture temporary = RenderTexture.GetTemporary(in_tex.width, in_tex.height, 0, RenderTextureFormat.Default, RenderTextureReadWrite.Linear);
+            Graphics.Blit(in_tex, temporary);
+            RenderTexture active = RenderTexture.active;
+            RenderTexture.active = temporary;
+            Texture2D texture2D = new Texture2D(in_tex.width, in_tex.height);
+            texture2D.ReadPixels(new Rect(0f, 0f, (float)temporary.width, (float)temporary.height), 0, 0);
+            texture2D.Apply();
+            RenderTexture.active = active;
+            RenderTexture.ReleaseTemporary(temporary);
+            return texture2D;
+        }
+        public static Texture2D SubTexture(Texture2D in_tex, RectP rectP)
+        {
+            Texture2D texture2D = new Texture2D(rectP.width, rectP.height);
+            texture2D.SetPixels(in_tex.GetPixels(rectP.x, rectP.y, rectP.width, rectP.height));
+            texture2D.Apply();
+            return texture2D;
+        }
+        public static Texture2D SubTexturePosition(Texture2D in_tex, RectP rectP)
+        {
+            Texture2D texture2D = new Texture2D(in_tex.width, in_tex.height);
+            texture2D.SetPixels(colors);
+            texture2D.SetPixels(rectP.x, rectP.y, rectP.width, rectP.height, in_tex.GetPixels(rectP.x, rectP.y, rectP.width, rectP.height));
+            texture2D.Apply();
+            return texture2D;
+        }
+        public static Texture2D SpriteSizeFix(Texture2D in_tex, RectP rectP, RectP border)
+        {
+            Texture2D texture2D = new Texture2D(border.width, border.height);
+            texture2D.SetPixels(colors);
+            texture2D.SetPixels(rectP.x, rectP.y, rectP.width, rectP.height, in_tex.GetPixels());
+            if (true) // GODump.instance.GlobalSettings.RedRectangular
+            {
+                for (int i = 0; i < texture2D.width; i++)
+                {
+                    for (int j = 0; j < texture2D.height; j++)
+                    {
+                        if (((i == rectP.xmin - 1 || i == rectP.xmax + 1) && (rectP.ymin - 1 <= j && j <= rectP.ymax + 1)) || ((rectP.xmin - 1 <= i && i <= rectP.xmax + 1) && (j == rectP.ymin - 1 || j == rectP.ymax + 1)))
+                        {
+                            texture2D.SetPixel(i, j, Color.red);
+                        }
+                    }
+                }
+            }
+            texture2D.Apply();
+            return texture2D;
+        }
+
+        public static Texture2D SpriteSizeFixError(Texture2D in_tex, RectP rectP, RectP border)
+        {
+            Texture2D texture2D = new Texture2D(border.width, border.height);
+            texture2D.SetPixels(colors);
+
+            // 檢查 rectP 是否超出 in_tex 或 texture2D 範圍
+            if (rectP.x < 0 || rectP.y < 0 ||
+                rectP.x + rectP.width > in_tex.width ||
+                rectP.y + rectP.height > in_tex.height ||
+                rectP.x + rectP.width > texture2D.width ||
+                rectP.y + rectP.height > texture2D.height)
+            {
+                GoDump.GoDump.logInfo($"Invalid rectP bounds in SpriteSizeFix for rectP: x={rectP.x}, y={rectP.y}, width={rectP.width}, height={rectP.height}. Adjusting.");
+                //GoDump.GoDump.Instance.Logger.LogWarning($"Invalid rectP bounds in SpriteSizeFix for rectP: x={rectP.x}, y={rectP.y}, width={rectP.width}, height={rectP.height}. Adjusting.");
+                // 裁剪 rectP 範圍以確保不超出紋理尺寸
+                rectP.x = Mathf.Max(0, rectP.x);
+                rectP.y = Mathf.Max(0, rectP.y);
+                rectP.width = Mathf.Min(rectP.width, Mathf.Min(in_tex.width - rectP.x, texture2D.width - rectP.x));
+                rectP.height = Mathf.Min(rectP.height, Mathf.Min(in_tex.height - rectP.y, texture2D.height - rectP.y));
+
+                // 如果裁剪後寬高無效，則返回空紋理或記錄錯誤
+                if (rectP.width <= 0 || rectP.height <= 0)
+                {
+                    GoDump.GoDump.logInfo($"Invalid rectP after adjustment: width={rectP.width}, height={rectP.height}. Returning empty texture.");
+                    //Logger.LogError($"Invalid rectP after adjustment: width={rectP.width}, height={rectP.height}. Returning empty texture.");
+                    return texture2D;
+                }
+            }
+
+            texture2D.SetPixels(rectP.x, rectP.y, rectP.width, rectP.height, in_tex.GetPixels(0, 0, rectP.width, rectP.height));
+            if (true) // GODump.instance.GlobalSettings.RedRectangular
+            {
+                for (int i = 0; i < texture2D.width; i++)
+                {
+                    for (int j = 0; j < texture2D.height; j++)
+                    {
+                        if (((i == rectP.xmin - 1 || i == rectP.xmax + 1) && (rectP.ymin - 1 <= j && j <= rectP.ymax + 1)) ||
+                            ((rectP.xmin - 1 <= i && i <= rectP.xmax + 1) && (j == rectP.ymin - 1 || j == rectP.ymax + 1)))
+                        {
+                            texture2D.SetPixel(i, j, Color.red);
+                        }
+                    }
+                }
+            }
+            texture2D.Apply();
+            return texture2D;
+        }
+
+        public static void SaveTextureToFile(Texture2D tex, string fileName)
+        {
+            byte[] buffer = tex.EncodeToPNG();
+            Directory.CreateDirectory(Path.GetDirectoryName(fileName));
+            FileStream output = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite);
+            BinaryWriter binaryWriter = new BinaryWriter(output);
+            binaryWriter.Write(buffer);
+            binaryWriter.Close();
+        }
+        public struct RectP
+        {
+            public int x { set; get; }
+            public int y { set; get; }
+            public int width { set; get; }
+            public int height { set; get; }
+            public int xmin { set; get; }
+            public int ymin { set; get; }
+            public int xmax { set; get; }
+            public int ymax { set; get; }
+
+            public RectP(int _x, int _y, int _width, int _height)
+            {
+                x = _x;
+                y = _y;
+                width = _width;
+                height = _height;
+                xmin = _x;
+                ymin = _y;
+                xmax = _x + _width - 1;
+                ymax = _y + _height - 1;
+            }
+        }
+    }
+}
